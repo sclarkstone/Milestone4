3b6d0dfe974ab59c97fd2593858f80f98b374eb9
sclarkstone <samclarkstone@hotmail.com>
2022-02-05 13:35:51 +0000

    Add shopping bag app, urls and template

--- home/views.py
+++ home/views.py
@@ -3,6 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    """ A view ro return the index page """
+    """ A view to return the index page """
   
     return render(request, 'home/index.html')
