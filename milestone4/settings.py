dfce7598cbbce1a80c4bab26c3bf978e65619800
sclarkstone <samclarkstone@hotmail.com>
2022-02-19 17:02:03 +0000

    create postgres db

--- milestone4/settings.py
+++ milestone4/settings.py
@@ -1,3 +1,4 @@
+import dj_database_url
 import os
 """
 Django settings for milestone4 project.
@@ -123,7 +124,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
