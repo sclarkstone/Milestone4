7970bb6b6e728ae2c34146bea2cd14243ab038bb
sclarkstone <samclarkstone@hotmail.com>
2022-03-22 14:19:55 +0000

    added missing call to db for heroku

--- milestone4/settings.py
+++ milestone4/settings.py
@@ -124,7 +124,7 @@ WSGI_APPLICATION = 'milestone4.wsgi.application'
 
 if 'DATABASE_URL' in os.environ:
     DATABASES = {
-        'default': dj_database_url.parse('')
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
 else:
     DATABASES = {
